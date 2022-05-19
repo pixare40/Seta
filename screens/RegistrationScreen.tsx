e5e33c626854287926e253d70d598ab14367b82e
Kabaji Egara <kabzegara@gmail.com>
2022-05-19 23:08:25 +0100

    added react navigation

---
+++ screens/RegistrationScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const RegistrationScreen = () => {
+    return (
+        <View>
+            <Text>
+                Registration Screen
+            </Text>
+        </View>
+    )
+}
+
+export default RegistrationScreen;
