d107c3ee2563945c9b8f87b71b03a03890bebbe0
Kabaji Egara <kabzegara@gmail.com>
2022-11-26 15:20:14 +0000

    testing signing

--- components/Specials/Specials.tsx
+++ components/Specials/Specials.tsx
@@ -9,6 +9,8 @@ const Specials = () => {
 			<View style={styles.specials} />
 			<View style={styles.specials} />
 			<View style={styles.specials} />
+			<View style={styles.specials} />
+			<View style={styles.specials} />
 		</View>
 	);
 };
